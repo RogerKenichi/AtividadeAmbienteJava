fce8b8ad94f81b865ed05bef0b0ffc1014177104
RogerKenichi <rogerkenichi003@gmail.com>
2021-08-23 22:12:10 -0300

    Adicionado classe de salvar e ler json,
    Inclui código de exemplo na classe main()

--- AtividadeAmbienteJava/src/com/company/Main.java
+++ AtividadeAmbienteJava/src/com/company/Main.java
@@ -1,6 +1,12 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    private static List<Tarefa> tarefas = new ArrayList<Tarefa>();
+    private static List<Classificacao> classificacoes = new ArrayList<Classificacao>();
 
     public static void main(String[] args) {
         int resp = 0;
@@ -17,5 +23,46 @@ public class Main {
                     break;
             }
         }while(resp != 2);
+
+
+
+        // ------------------------------ Teste de demonstração ---------------------------
+        // Objeto tarefa
+        Tarefa tarefa = new Tarefa();
+        tarefa.setId(2);
+        tarefa.setNome("teste1");
+        tarefa.setData("12/04/2021");
+        tarefa.setClassificação(0);
+        tarefa.setDetalhes("Tem que fazer algo");
+
+        // adicionando tarefas a lista tarefas
+        tarefas.add(tarefa);
+        tarefas.add(tarefa);
+        tarefas.add(tarefa);
+
+        // Objeto classificação
+        Classificacao cla = new Classificacao();
+        cla.setId(5);
+        cla.setNomeClassificacao("domestico");
+
+        // adicionando classificações a lista classificações
+        classificacoes.add(cla);
+        classificacoes.add(cla);
+
+        // Salvando o json
+        SalvamentoJson salvar = new SalvamentoJson(tarefas, classificacoes);
+        salvar.salvarJsonTarefas();
+
+        // Lendo o json
+        salvar.lerJsonTarefas();
+
+        // Carregando os valores lidos para as listas de Main()
+        classificacoes = salvar.getClassificacao();
+        tarefas = salvar.getTarefas();
+
+        // Printando alguns valores para mostrar que funciona
+        System.out.println(classificacoes.get(1).getNomeClassificacao());
+        System.out.println(tarefas.get(1).getNome());
+        // ------------------------------------------------------------------------
     }
 }
---
+++ AtividadeAmbienteJava/src/com/company/SalvamentoJson.java
@@ -0,0 +1,122 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+public class SalvamentoJson {
+
+    private String filepath = "./tarefas.json";
+    private List<Tarefa> listaTarefas = new ArrayList<Tarefa>();
+    private List<Classificacao> listaClassificacao = new ArrayList<Classificacao>();
+
+    public List<Tarefa> getTarefas() { return this.listaTarefas; }
+    public List<Classificacao> getClassificacao() { return this.listaClassificacao; }
+
+    public  SalvamentoJson(List<Tarefa> tarefas, List<Classificacao> classificacoes)
+    {
+        this.listaTarefas = tarefas;
+        this.listaClassificacao = classificacoes;
+    }
+
+    public void salvarJsonTarefas()
+    {
+        JSONObject salvamentoTarefas = new JSONObject();
+        JSONArray tarefas = new JSONArray();
+        JSONArray classificacoes = new JSONArray();
+
+        // Adiciona as tarefas para o objeto tarefas do json
+        for(Tarefa t : this.listaTarefas)
+        {
+            JSONObject objTarefa = new JSONObject();
+            objTarefa.put("id", t.getId());
+            objTarefa.put("nome", t.getNome());
+            objTarefa.put("detalhes", t.getDetalhes());
+            objTarefa.put("data", t.getData());
+            objTarefa.put("classificacao", t.getClassificação());
+
+            tarefas.add(objTarefa);
+        }
+
+        // Adiciona as classificações para o objeto classificacoes do json
+        for(Classificacao c : this.listaClassificacao)
+        {
+            JSONObject objClassificacoes = new JSONObject();
+            objClassificacoes.put("id", c.getId());
+            objClassificacoes.put("nomeclassificacao", c.getNomeClassificacao());
+
+            classificacoes.add(objClassificacoes);
+        }
+
+        // Cria o objeto tarefas e o objeto classificacoes
+        salvamentoTarefas.put("tarefas", tarefas);
+        salvamentoTarefas.put("classificacoes", classificacoes);
+
+        // Salva o arquivo
+        try (FileWriter file = new FileWriter(this.filepath)) {
+            file.write(salvamentoTarefas.toJSONString());
+            file.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void lerJsonTarefas()
+    {
+        JSONParser jsonParser = new JSONParser();
+
+        try (FileReader reader = new FileReader(this.filepath))
+        {
+            this.listaTarefas.clear();
+            this.listaClassificacao.clear();
+
+            Object obj = jsonParser.parse(reader);
+
+            // Lê o objeto tarefas
+            JSONObject tarefas = (JSONObject) obj;
+            JSONArray listaTarefas = (JSONArray) tarefas.get("tarefas");
+            for (Object t : listaTarefas) {
+                JSONObject tf = (JSONObject) t;
+                Tarefa objTarefa = new Tarefa();
+
+                objTarefa.setId(((Long) tf.get("id")).intValue());
+                objTarefa.setNome((String) tf.get("nome"));
+                objTarefa.setData((String) tf.get("data"));
+                objTarefa.setDetalhes((String) tf.get("detalhes"));
+                objTarefa.setClassificação(((Long) tf.get("classificacao")).intValue());
+
+                this.listaTarefas.add(objTarefa);
+            }
+
+            // Lê o objeto classificações
+            JSONObject classificacoes = (JSONObject) obj;
+            JSONArray listaClass = (JSONArray) classificacoes.get("classificacoes");
+            for (Object t : listaClass) {
+                JSONObject tf = (JSONObject) t;
+                Classificacao objClassificacao = new Classificacao();
+
+                objClassificacao.setId(((Long) tf.get("id")).intValue());
+                objClassificacao.setNomeClassificacao((String) tf.get("nomeclassificacao"));
+
+                this.listaClassificacao.add(objClassificacao);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+}
